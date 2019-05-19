b3b9a47fd9d9eec8a805fc39bde23d5561578ee8
wangruinan0826 <nanjoker0826@126.com>
2019-05-19 13:58:37 +0800

    前端开发

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
+import A from './components/A.js'
+import B from './components/B.js'
+import C from './components/C.js'
+import Home from './components/Home.js'
 function App() {
   return (
     <div className="App">
-      
+        hahaha
+        <Router>
+            <Link to='/'>首页</Link>
+            -------
+            <Link to='/a/5'>A页面</Link>
+            -------
+            <Link to='/b'>B页面</Link>
+            -------
+            <Link to='/c/777'>C页面</Link>
+            <Route exact path='/' component={Home}></Route>
+            <Route path='/a/:id' component={A}></Route>
+            <Route path='/b' component={B}></Route>
+            <Route path='/c/:num' component={C}></Route>
+            {/* <Switch>
+              <Route path='/a/:id' component={A}></Route>
+              <Route path='/b' component={B}></Route>
+              <Route path='/' component={Home}></Route>
+            </Switch> */}
+        </Router>
     </div>
   );
 }
---
+++ react-app/src/components/C.js
@@ -0,0 +1,8 @@
+import React from 'react'
+let C=({match})=>{
+    console.log(match.params.num)
+    return(
+        <div>CCCCCCC</div>
+    )
+}
+export default C;
