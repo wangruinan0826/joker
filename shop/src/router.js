96542502b32606af5f37558f9f05e2553ed5df3c
wangruinan0826 <nanjoker0826@126.com>
2019-07-06 08:54:25 +0800

    前端开发

--- shop/src/router.js
+++ shop/src/router.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Category from './views/Category.vue'
+import Cart from './views/Cart.vue'
+import Profile from './views/Profile.vue'
 
 Vue.use(Router)
 
@@ -7,6 +11,25 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    
+    {
+      path:'/',
+      name:'home',
+      component:Home
+    },
+    {
+      path:'/category',
+      name:'category',
+      component:Category
+    },
+    {
+      path:'/cart',
+      name:'cart',
+      component:Cart
+    },
+    {
+      path:'/profile',
+      name:'profile',
+      component:Profile
+    },
   ]
 })
